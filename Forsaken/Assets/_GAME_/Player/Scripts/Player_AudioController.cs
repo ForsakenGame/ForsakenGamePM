26bbf450115465f27833e308735a295f9b2db32e
alexsugimoto <alexsugimoto@outlook.com>
2025-02-07 17:15:03 +0100

    feature: shooting sound added

--- Forsaken/Assets/_GAME_/Player/Scripts/Player_AudioController.cs
+++ Forsaken/Assets/_GAME_/Player/Scripts/Player_AudioController.cs
@@ -10,12 +10,26 @@ public class Player_AudioController : MonoBehaviour
     public AudioClip footstep3;
     public AudioClip footstep4;
     public AudioClip footstep5;
+    public AudioClip machinegun;
 
-    private List<AudioClip> footsteps;
+    private bool _isRightButtonHeld;
+    private AudioSource _shootingAudioSource;
+    private List<AudioClip> _footsteps;
+    private Animator _animator;
+
+    private readonly List<string> shootingAnimations = new List<string>
+    {
+        "Anim_Player_RunningGun_Down",
+        "Anim_Player_RunningGun_Right",
+        "Anim_Player_RunningGun_Up",
+        "Anim_Player_StaticGun_Down",
+        "Anim_Player_StaticGun_Right",
+        "Anim_Player_StaticGun_Up"
+    };
 
     private void Start()
     {
-        footsteps = new List<AudioClip>
+        _footsteps = new List<AudioClip>
         {
             footstep1,
             footstep2,
@@ -23,14 +37,77 @@ public class Player_AudioController : MonoBehaviour
             footstep4,
             footstep5
         };
+
+        // AudioSource for shooting
+        _shootingAudioSource = gameObject.AddComponent<AudioSource>();
+        _shootingAudioSource.clip = machinegun;
+        _shootingAudioSource.loop = true; // Make it loop
+        _shootingAudioSource.playOnAwake = false;
+        _shootingAudioSource.volume = 0.1f;
+
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        GatherInput();
+    }
+
+    private void GatherInput()
+    {
+        // Avoids shooting sound playing when right clicking on incorrect animation 
+        bool isShooting = Input.GetMouseButton(1) && IsShootingAnimationPlaying(); 
+
+        if (isShooting)
+        {
+            PlayShootingSound();
+        }
+        else 
+        {
+            StopShootingSound();
+        }
     }
 
     public void PlayRandomFootstep()
     {
         System.Random random = new System.Random();
-        int i = random.Next(1, footsteps.Count); // Generate a random number to choose a random footstep each time
-        AudioClip footstep = footsteps[i]; // Retrieve random footstep
+        int i = random.Next(1, _footsteps.Count);
+        AudioClip footstep = _footsteps[i];
+
         AudioManager.instance.enabled = true;
         AudioManager.instance.PlayAudio(footstep, "footstep" + i, 0.1f, false);
+    }
+
+    public void PlayShootingSound()
+    {
+        if (!_shootingAudioSource.isPlaying) // Avoid audio stacking
+        {
+            _shootingAudioSource.Play();
+        }
+    }
+
+    public void StopShootingSound()
+    {
+        if (_shootingAudioSource.isPlaying)
+        {
+            _shootingAudioSource.Stop();
+        }
+    }
+
+    private bool IsShootingAnimationPlaying()
+    {
+        AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+
+        foreach (string animationName in shootingAnimations)
+        {
+            if (stateInfo.IsName(animationName))
+            {
+                // Debug.Log("shooting animation is playing");
+                // Debug.Log(stateInfo.GetHashCode());
+                return true;
+            }
+        }
+        // Debug.Log("quack, quack");
+        return false;
     }
 }
