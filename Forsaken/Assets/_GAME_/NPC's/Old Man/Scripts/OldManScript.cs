9b81cbb697b93d3c7459668d5b0d019b0f22ca1d
batistaker47 <david.pires1997@gmail.com>
2025-02-15 12:18:57 +0100

    feat: Dialogues between NPC and player finished and working

--- Forsaken/Assets/_GAME_/NPC's/Old Man/Scripts/OldManScript.cs
+++ Forsaken/Assets/_GAME_/NPC's/Old Man/Scripts/OldManScript.cs
@@ -1,14 +1,68 @@
 using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class OldManScript : MonoBehaviour
 {
     private bool isPlayerInRange;
+    private bool didDialogueStart;
+    private int lineIndex;
+    private float typingTime = 0.1f;
+
+
     public GameObject dialogueMark;
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText;
+    public string[] dialogues;
 
     void Update()
     {
-        
+        if(isPlayerInRange && Input.GetButtonDown("Fire1"))
+        {
+            if(!didDialogueStart)
+            StartDialogue();
+        }
+        else if(dialogueText.text == dialogues[lineIndex])
+        {
+            NextDialogueLine();
+        }
+    }
+
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true);
+        dialogueMark.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(ShowLine());
+    }
+
+    private void NextDialogueLine()
+    {
+        lineIndex++;
+        if(lineIndex < dialogues.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            didDialogueStart = false;
+            dialoguePanel.SetActive(false);
+            dialogueMark.SetActive(true);
+            Time.timeScale = 1f;
+        }
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+
+        foreach(char c in dialogues[lineIndex])
+        {
+            dialogueText.text += c;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
